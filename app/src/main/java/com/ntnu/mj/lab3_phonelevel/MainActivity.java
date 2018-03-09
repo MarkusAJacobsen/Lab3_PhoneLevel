d6892809f422603ffca990bd95a943fce4faf756
MarkusJacobsen <markusja@stud.ntnu.no>
2018-03-09 12:41:03 +0100

    Fence and bouble works
    Ball stays within the fence and gets a pushback upon hitting the fence

--- app/src/main/java/com/ntnu/mj/lab3_phonelevel/MainActivity.java
+++ app/src/main/java/com/ntnu/mj/lab3_phonelevel/MainActivity.java
@@ -1,13 +1,101 @@
 package com.ntnu.mj.lab3_phonelevel;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
+/**
+ * Main activity
+ */
 public class MainActivity extends AppCompatActivity {
+    SensorManager manager;
+    Sensor sensor;
+    SceneLevel ball;
+    LevelListener listener;
 
+    /**
+     * Called when the application launches
+     * @param savedInstanceState Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Remove the actionbar and run in fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final ActionBar mActionBar = getSupportActionBar();
+        mActionBar.hide();
+
+
+
+        //Fetch accelerometer
+        manager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Main view displaying the animation
+        ball = new SceneLevel(getApplicationContext());
+
+        //Listener for the sensor manager
+        listener = new LevelListener(ball);
+
+        //Set content view to the custom view
+        setContentView(ball);
     }
+
+    /**
+     * Called after onCreate, registers the listener for the accelerometer
+     */
+    @Override
+    public void onResume(){
+        super.onResume();
+        manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    /**
+     * If the view is paused, decouple the listener for the accelerometer
+     */
+    @Override
+    public void onPause(){
+        super.onPause();
+        manager.unregisterListener(listener);
+    }
+
+
+    /**
+     * Listener for the accelerometer, calls SceneLevel's moveBall with received event
+     */
+    private class LevelListener implements SensorEventListener {
+        private SceneLevel level;
+
+        LevelListener(SceneLevel level) {
+            this.level = level;
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            level.moveBall(sensorEvent);
+        }
+
+        /** Not used, NOOP
+         * @param sensor Sensor
+         * @param i int
+         */
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/ntnu/mj/lab3_phonelevel/SceneLevel.java
@@ -0,0 +1,162 @@
+package com.ntnu.mj.lab3_phonelevel;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.SensorEvent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Vibrator;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ * Created by markusja on 3/2/18.
+ */
+
+/**
+ * Extended view responsible of drawing the bubble and fence continually with the move logic
+ */
+public class SceneLevel extends View {
+    private Paint paint;
+    Context context;
+
+    //Dimensions
+    private int width;
+    private int height;
+    private final int margin = 40;
+    private final int stroke  =30;
+
+    //Fence
+    private Rect fence;
+    private Paint fencePaint;
+
+    //Move variables
+    private int xUpperBoundary;
+    private int yUpperBoundary;
+    private int bothLowerBoundary;
+    private final int step = 4;
+    private final int pushBack = 100;
+
+    //Bubble
+    private int bubbleX;
+    private int bubbleY;
+    private final int bubbleRadius  =20;
+    private Paint bubblePaint;
+
+    /**
+     * Constructor, initialize all the different graphical elements which later are displayed
+     * @param context Context
+     */
+    public SceneLevel(Context context) {
+        super(context);
+        this.context = context;
+
+        fetchWindowDimensions();
+
+        //Fence setup
+        fence = new Rect(margin, margin, width-margin, height-margin);
+        fencePaint = new Paint();
+        fencePaint.setColor(Color.BLACK);
+        fencePaint.setStrokeWidth(stroke);
+        fencePaint.setStyle(Paint.Style.STROKE);
+        xUpperBoundary = width-margin-stroke;
+        yUpperBoundary = height-margin-stroke;
+        bothLowerBoundary = 0+margin+stroke;
+
+        //Bubble setup
+        bubbleX = width/2;
+        bubbleY = height/2;
+        bubblePaint = new Paint();
+        bubblePaint.setColor(Color.RED);
+    }
+
+    /**
+     * https://stackoverflow.com/a/1016941/7036624
+     */
+    private void fetchWindowDimensions(){
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
+    }
+
+    /**
+     * Overrided draw function. Draw the bubble and fence. Will upon every cycle invalidate the
+     * view such that the draw function will always be called and continually updating the view
+     * @param canvas Canvas
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawRect(fence, fencePaint);
+        canvas.drawCircle(bubbleX, bubbleY, bubbleRadius, bubblePaint);
+        invalidate();
+    }
+
+    /**
+     * Move logic for the ball. Checks if the ball has collided with the fence.
+     * If so apply a small "bounce" to the ball, the bounce is in reality more of a pushback
+     * On collision trigger a vibrate and a ping
+     * @param event SensorEvent
+     */
+    public void moveBall(SensorEvent event){
+        int xCheck =  bubbleX + (int) event.values[1];
+        int yCheck = bubbleY + (int) event.values[0];
+
+        //X axis logic
+        if(xCheck >= xUpperBoundary) {
+            bubbleX = bubbleX-pushBack;
+            vibrate();
+        } else if(xCheck <= bothLowerBoundary) {
+            bubbleX = bubbleX+pushBack;
+            vibrate();
+        } else {
+            bubbleX = (int) (bubbleX+step*(1*event.values[1]));
+        }
+
+        //Y axis logic
+        if(yCheck >= yUpperBoundary) {
+            bubbleY = bubbleY-pushBack;
+            vibrate();
+            ping();
+        } else if(yCheck <= bothLowerBoundary) {
+            bubbleY = bubbleY+pushBack;
+            vibrate();
+            ping();
+        } else {
+            bubbleY = (int) (bubbleY+step*(1*event.values[0]));
+        }
+
+    }
+
+    /**
+     * https://stackoverflow.com/questions/13950338/how-to-make-an-android-device-vibrate
+     */
+    private void vibrate(){
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(100);
+    }
+
+    /**
+     *  https://stackoverflow.com/questions/4441334/how-to-play-an-android-notification-sound/9622040#9622040
+     */
+
+    private void ping(){
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(context, notification);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
